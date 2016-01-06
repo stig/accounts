3ed05c42cc66528594240fe5d26bc0a7e243a0a4
Stig Brautaset <stig@brautaset.org>
2016-01-06 00:54:06 +0000

    Present dirt-simple login form + accept POST parameters

--- src/accounts/endpoint/login.clj
+++ src/accounts/endpoint/login.clj
@@ -1,9 +1,27 @@
 (ns accounts.endpoint.login
   (:require [accounts.layout :as layout]
             [compojure.core :refer :all]
-            [hiccup.page :refer [html5]]))
+            [hiccup
+             [form :refer [email-field form-to submit-button]]
+             [page :refer [html5]]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+
+(defn login-form []
+  (list
+   [:h1 "Login"]
+   (form-to [:post "/login"]
+            (anti-forgery-field)
+            (email-field :email)
+            (submit-button :submit))))
+
 
 (defn login-endpoint [config]
   (context "/login" []
+           (POST "/" [email]
+                 (html5 (layout/base
+                         {:content
+                          (list [:h1 "Email"]
+                                [:p email])})))
            (GET "/" []
-                (html5 (layout/base {:content [:p "Hello from Login Page"]})))))
+                (html5 (layout/base
+                        {:content (login-form)})))))
--- src/accounts/main.clj
+++ src/accounts/main.clj
@@ -1,14 +1,20 @@
 (ns accounts.main
   (:gen-class)
-  (:require [clojure.java.io :as io]
+  (:require [accounts
+             [config :as config]
+             [system :refer [new-system]]]
+            [clojure.java.io :as io]
             [com.stuartsierra.component :as component]
             [duct.middleware.errors :refer [wrap-hide-errors]]
             [meta-merge.core :refer [meta-merge]]
-            [accounts.config :as config]
-            [accounts.system :refer [new-system]]))
+            [ring.middleware
+             [anti-forgery :refer [wrap-anti-forgery]]
+             [params :refer [wrap-params]]]))
 
 (def prod-config
-  {:app {:middleware     [[wrap-hide-errors :internal-error]]
+  {:app {:middleware     [wrap-params
+                          wrap-anti-forgery
+                          [wrap-hide-errors :internal-error]]
          :internal-error (io/resource "errors/500.html")}})
 
 (def config
