6db485c158d687c0eb7eb145e2fefafda1420d4b
Stig Brautaset <stig@brautaset.org>
2016-01-23 22:39:04 +0000

    Added a couple basic db tests

--- src/accounts/users.clj
+++ src/accounts/users.clj
@@ -1,8 +1,25 @@
 (ns accounts.users
   (:require [clojure.java.jdbc :as j]))
 
+(def ^:private last-insert-rowid (keyword "last_insert_rowid()"))
+
+(defn add
+  "Add a user to the db"
+  [{spec :spec} user]
+  (some-> (j/insert! spec :users user)
+          first
+          last-insert-rowid))
+
+(defn find-by-id
+  "find a user by their id"
+  [{spec :spec} id]
+  (j/query spec
+           ["select * from users where id = ?" id]
+           :result-set-fn first))
+
 (defn find-by-email
+  "Find user by their email"
   [{spec :spec} email]
   (j/query spec
-           ["select id from users where email = ?" email]
+           ["select * from users where email = ?" email]
            :result-set-fn first))
--- test/accounts/users_test.clj
+++ test/accounts/users_test.clj
@@ -1,7 +1,27 @@
 (ns accounts.users-test
-  (:require [accounts.users :refer :all]
+  (:require [accounts.main :refer [config]]
+            [accounts.users :refer :all]
             [clojure.test :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def db {:spec {:classname   "org.sqlite.JDBC"
+                :subprotocol "sqlite"
+                :subname     "db/dev.sqlite"}})
+
+(deftest users-test
+  (testing "find non-existing user"
+    (is (nil? (find-by-email db (gensym))))
+    (is (nil? (find-by-id db (gensym)))))
+
+  (testing "adding user"
+    (is (number? (add db {:email (gensym)
+                          :moniker (gensym)}))))
+
+  (testing "retrieving added user"
+    (let [email (str (gensym))
+          id (add db {:email email :moniker "Known Name"})
+          user {:id id :email email :moniker "Known Name"}]
+
+      (is (= user (find-by-email db email)))
+      (is (= user (find-by-id db id))))
+    )
+  )
