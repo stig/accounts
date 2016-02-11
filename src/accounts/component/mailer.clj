9df6f84416535774405f84b9e90489f8c2544d5a
Stig Brautaset <stig@brautaset.org>
2016-02-11 23:55:06 +0000

    Fix issue requiring (reset) before app would work
    This seems to somehow be related to protocols. I don't understart how,
    but moving to a multimethod seems to have solved it. It's a shit fix,
    but it works. And I got to learn about multimethods :-)

--- src/accounts/component/mailer.clj
+++ src/accounts/component/mailer.clj
@@ -3,36 +3,35 @@
             [clojure.core.async :refer [>!! chan close!]]
             [postal.core :refer [send-message]]))
 
-(defprotocol Mailer
-  (mail [mail-service to subject body]))
+(defmulti mail (fn [x & _] (class x)))
 
 (defrecord SmtpMailer [smtp-config]
-  Mailer
   component/Lifecycle
   (start [this] this)
-  (stop [this] this)
-  (mail [{smtp-config :smtp-config} to subject body]
-    (send-message smtp-config
-                  {:from "accounts@superloopy.io"
-                   :to to
-                   :subject subject
-                   :body body})))
+  (stop [this] this))
+
+(defmethod mail SmtpMailer [{:keys [smtp-config]} to subject body]
+  (send-message smtp-config
+                {:from "accounts@superloopy.io"
+                 :to to
+                 :subject subject
+                 :body body}))
 
 (defn mailer [smtp-config]
   (->SmtpMailer smtp-config))
 
+
 (defrecord StubMailer [channel]
-  Mailer
   component/Lifecycle
   (start [this]
     (assoc this :channel (chan 32)))
 
   (stop [this]
     (close! (:channel this))
-    (dissoc this :channel))
+    (dissoc this :channel)))
 
-  (mail [_ to subject body]
-    (>!! channel {:to to :subject subject :body body})))
+(defmethod mail StubMailer [{:keys [channel]} to subject body]
+  (>!! channel {:to to :subject subject :body body}))
 
 (defn stub-mailer []
   (->StubMailer (chan 32)))
