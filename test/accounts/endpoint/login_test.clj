d9c417b0ce3fedf7ef3a377b47e105e169f398bd
Stig Brautaset <stig@brautaset.org>
2016-02-14 13:05:00 +0000

    Use clojure.test's fixture setup rather than our own ad-hoc one

--- test/accounts/endpoint/login_test.clj
+++ test/accounts/endpoint/login_test.clj
@@ -13,6 +13,8 @@
 
 (def config {:db {:uri "jdbc:sqlite::memory:"}})
 
+(defn handler [] (some-> system :app :handler))
+
 (defn test-system [config]
   (-> (new-system config)
 
@@ -22,50 +24,53 @@
       ;; Don't actually send email: use our test mailer instead
       (assoc :mailer (stub-mailer))))
 
+(def system (test-system config))
+
+(defn my-fixture [f]
+  (alter-var-root #'system (fn [x] (component/start x)))
+  (migrate (:ragtime system))
+  (f)
+  (alter-var-root #'system (fn [x] (component/stop x))))
+
+(use-fixtures :once my-fixture)
+
 (deftest smoke-test
-  (let [system (component/start (test-system config))
-        handler (some-> system :app :handler)]
-    (try
-      (migrate (:ragtime system))
+  (testing "login page exists"
+    (-> (session (handler))
+        (visit "/login")
+        (has (status? 200) "page exists")))
 
-      (testing "login page exists"
-        (-> (session handler)
-            (visit "/login")
-            (has (status? 200) "page exists")))
+  (testing "bad email not found"
+    (-> (session (handler))
+        (visit "/login")
+        (fill-in "Email:" "notvalid@example.com")
+        (press "submit")
+        (within [:h1]
+                (has (text? "User not found")))))
 
-      (testing "bad email not found"
-        (-> (session handler)
-            (visit "/login")
-            (fill-in "Email:" "notvalid@example.com")
-            (press "submit")
-            (within [:h1]
-                    (has (text? "User not found")))))
+  (testing "login success"
+    ;; We don't support registering yet, so manually add a test user
+    ;; before the login attempt.
+    (let [email (str (gensym) "@example.com")
+          db-spec (-> system :db :spec)
+          channel (-> system :mailer :channel)
+          user-id (add db-spec {:email email :moniker email})]
+      (-> (session (handler))
+          (visit "/login")
+          (fill-in "Email:" email)
+          (press "submit")
+          (within [:h1]
+                  (has (text? "Login token on its way!"))))
 
-      (testing "login success"
-        ;; We don't support registering yet, so manually add a test user
-        ;; before the login attempt.
-        (let [email (str (gensym) "@example.com")
-              db-spec (-> system :db :spec)
-              channel (-> system :mailer :channel)
-              user-id (add db-spec {:email email :moniker email})]
-          (-> (session handler)
-              (visit "/login")
-              (fill-in "Email:" email)
-              (press "submit")
-              (within [:h1]
-                      (has (text? "Login token on its way!"))))
-
-          (let [m (<!! channel)]
-            (is (= email (:to m)))
-            (is (= "One-time login URL" (:subject m)))
+      (let [m (<!! channel)]
+        (is (= email (:to m)))
+        (is (= "One-time login URL" (:subject m)))
             ;;; TODO check link in body
-            )
+        )
 
-          ;; We want to check there's no more messages on the channel, however
-          ;; <!! blocks if there's no message, so write a sentinel and make
-          ;; sure that's what we get back.
-          (let [sentinel (keyword (gensym))]
-            (is (= sentinel (do (>!! channel sentinel)
-                                (<!! channel)))))))
-
-      (finally (component/stop system)))))
+      ;; We want to check there's no more messages on the channel, however
+      ;; <!! blocks if there's no message, so write a sentinel and make
+      ;; sure that's what we get back.
+      (let [sentinel (keyword (gensym))]
+        (is (= sentinel (do (>!! channel sentinel)
+                            (<!! channel))))))))
